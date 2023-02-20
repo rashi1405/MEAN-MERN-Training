3cdcb90cdaa8849695a849b81102cc884f90bce1
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-20 23:00:36 +0530

    Add files via upload

---
+++ db-connect/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const url = 'mongodb://localhost/UserDB';
+
+const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url, { useNewUrlParser: true });
+const con = mongoose.connection;
+
+con.on('open', () => {
+  console.log('connected...');
+});
+
+app.use(express.json());
+
+const userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
+app.listen(9000, () => {
+  console.log('server started');
+});
