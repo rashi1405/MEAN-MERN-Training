d1d88374eeb002087b10d778b4b097abbb4e75b2
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-22 10:08:53 +0530

    Add files via upload

---
+++ middlewares/validator.js
@@ -0,0 +1,21 @@
+const { check, validationResult } = require('express-validator');
+
+exports.validateUser = [
+  check('name')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Name is Missing !')
+    .isLength({ min: 3, max: 20 })
+    .withMessage('Invalid name'),
+
+  check('email').normalizeEmail().isEmail().withMessage('Email is invalid'),
+  check('password').not().isEmpty().withMessage('Password is missing'),
+];
+
+exports.validate = (req, res, next) => {
+  const error = validationResult(req).array();
+
+  if (!error.length) return next();
+  res.status(400).json({ success: false, error: error[0].msg });
+};
