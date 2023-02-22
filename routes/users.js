c4f6e9f3c56b4a1da3b659409f1c4dd22d0d634a
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-22 10:15:36 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,21 @@
 const express = require('express');
-const{
-  addUser,
- // deleteUser,
-  getUserById,
+
+const {
   getUser,
+  getUserById,
+  addUser,
   updateUser,
-} from '../controllers/users.js';
+} = require('../controllers/users');
+const { validateUser, validate } = require('../middlewares/validator');
 
 const router = express.Router();
 const User = require('../models/user');
 
 router.get('/', getUser);
 
-router.post('/', addUser);
-
 router.get('/:id', getUserById);
+
+router.post('/', validateUser, validate, addUser);
 
 // router.delete('/:id', deleteUser);
 
