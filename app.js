69a92852637670449205ded6861d7551352c8bbe
Rashi Jaiswal <rashijaiswal1405@gmail.com>
2023-02-22 09:59:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 import express from 'express';
-
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost/UserDB';
 
-import usersRoutes from './routes/users.js';
-
 const app = express();
 const PORT = 5000;
-
 
 mongoose.set('strictQuery', false);
 mongoose.connect(url, { useNewUrlParser: true });
@@ -22,10 +18,8 @@ app.use(express.json());
 const userRouter = require('./routes/users');
 app.use('/users', usersRoutes);
 
-
 const bookRouter = require('./routes/books');
 app.use('/books', bookRouter);
-
 
 app.listen(PORT, () => {
   console.log(`server started on port:http://localhost:${PORT}`);
